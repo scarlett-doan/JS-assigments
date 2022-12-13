d4c2ce8ed7862f5e880167811d6767d827cbfe48
Scarlett Doan <scarlettdoan@Scarletts-Air.home>
2022-12-13 03:12:03 +0200

    Add #5 and #6

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 from 0 to 100
  */
 
-// const printNum = () => {
-//     for (let i = 0; i <= 100; i++) {
-//         setTimeout(() => console.log(i), 1000)
-//     }
-// }
+const printNum = () => {
+    for (let i = 0; i <= 100; i++) {
+        setTimeout(() => console.log(i), 1000)
+    }
+}
 
-// printNum()
+printNum()
 
 /*
 2. Given the array below:
@@ -22,23 +22,23 @@ You only need to produce the same array as expected result, no need to consider 
 possibility.
  */
 
-// let myArr = ['12-24-2014', '09-2022-23', '12-30-2021', '08-02-2021', '07-15-2018', '2019-12-14', '2022-14-12']
-// const fixDate = (array) => {
-//     const temp = [];
-//     array.forEach((item) => {
-//         let dateItem = item.split("-");
-//         dateItem.sort(function(a,b) {return a-b});
-//         let correct = [dateItem[1], dateItem[0], dateItem[2]];
-//         temp.push(correct.join("-"));
-//     });
-//     return temp;
-// }
-// let newArr = fixDate(myArr)
-// console.log(newArr);
+let myArr = ['12-24-2014', '09-2022-23', '12-30-2021', '08-02-2021', '07-15-2018', '2019-12-14', '2022-14-12']
+const fixDate = (array) => {
+    const temp = [];
+    array.forEach((item) => {
+        let dateItem = item.split("-");
+        dateItem.sort(function(a,b) {return a-b});
+        let correct = [dateItem[1], dateItem[0], dateItem[2]];
+        temp.push(correct.join("-"));
+    });
+    return temp;
+}
+let newArr = fixDate(myArr)
+console.log(newArr);
 
 /*
 3. Counter function
-Write a counter funtion to print out in console the time difference between 2 given date
+Write a counter function to print out in console the time difference between 2 given date
 Expected result in the console: 11 days - 13 hours - 38 minutes - 20 seconds
 */
 const dateFrom = new Date(500000)
@@ -48,8 +48,8 @@ const counter = (from, to) => {
     let day = dif.getUTCDay();
     let hour = dif.getUTCHours();
     let min = dif.getUTCMinutes();
-    let sed = dif.getUTCSeconds();
-    return `${day} days - ${hour} hours - ${min} minutes - ${sed} seconds`;
+    let sec = dif.getUTCSeconds();
+    return `${day} days - ${hour} hours - ${min} minutes - ${sec} seconds`;
 }
 const timer = counter(dateFrom, dateTo)
 console.log(timer)
@@ -78,39 +78,115 @@ If folder 'New Folder' exists, it should add 'New Folder (1)' to array. If 'New 
 to array, and so on.
 */
 
-// const generateNewFolderName = (existingFolders) => {
-//     /*  provide your code here */
-// }
+const generateNewFolderName = (existingFolders) => {
+    if(!existingFolders.includes("New Folder")){
+        existingFolders.push("New Folder");
+    }
+    else{
+        let count = 0;
+        for (const item of existingFolders) {
+            if(item.includes("New Folder")){
+                count+=1;
+            }
+        }
+        existingFolders.push(`New Folder (${count})`)
+    }
+}
 
-// let folder = []
-// generateNewFolderName(folder)
-// generateNewFolderName(folder)
-// generateNewFolderName(folder)
-// generateNewFolderName(folder)
-// console.log(folder); //expect to see ['New Folder', 'New Folder (1)', 'New Folder (2)', 'New Folder (3)']
+let folder = []
+generateNewFolderName(folder)
+generateNewFolderName(folder)
+generateNewFolderName(folder)
+generateNewFolderName(folder)
+console.log(folder); //expect to see ['New Folder', 'New Folder (1)', 'New Folder (2)', 'New Folder (3)']
 
 /* 
 6. Complete class Book:
-- class Book should have 3 properties: title (read-only, must be a string but cannot be empty), cost (private, must be positive number) and profit (private, positive number > 0 and =< 0.5)
+- class Book should have 3 properties: title (read-only, must be a string but cannot be empty),
+cost (private, must be positive number) and profit (private, positive number > 0 and =< 0.5)
 (error should be thrown if data is not valid)
 - give the logic to get book's price and profit separately.
 - give the logics to increase and decrease the price with a certain amount 
-- give the logic to calculate price based on cost and profit. For example: cost 14, profit 0.3 => expected price is 20.
+- give the logic to calculate price based on cost and profit.
+For example: cost 14, profit 0.3 => expected price is 20.
 
 Complete class TaxableBook: 
 - inherit Book, but have 1 more private parameter in the constructor: taxRate. 
 - give the logic to calculate price with taxRate. For example: 
 cost 14, profit 0.3 , tax 24% => expected price is 30.43
 */
-// class Book {
-//     _title
-//     constructor(title, cost, profit) {
-//     }
-// }
+class Book {
+    _title
+    _cost
+    _profit
+    _price
+    _profitAmount
+    constructor(title, cost, profit) {
+        if(typeof title != "string"){
+            throw "Title must be string";
+        }else if (title === ""){
+            throw "Title cannot be empty";
+        }else{
+            this._title = title;
+        }
 
-// class TaxableBook {
-//     /* provide your code here */
-// }
+        if (typeof cost != "number"){
+            throw "Cost must be number";
+        }else if (cost <= 0){
+            throw "Cost must be positive";
+        }else {
+            this._cost = cost;
+        }
 
-// const book1 = new Book("The Power of Habits", 14, 0.3)
-// const book2 = new TaxableBook("The Power of Habits", 14, 0.3, 24)
+        if (typeof profit != "number"){
+            throw "Profit must be number";
+        }else if (profit < 0 || profit > 0.5){
+            throw "Profit must be >0 and <=0.5";
+        }else {
+            this._profit = profit;
+        }
+
+        this._price = this._cost/(1-this._profit);
+
+    };
+
+    getPrice(){
+        return `The selling price is ${this._price}`;
+    }
+    getProfit(){
+        this._profitAmount = this._price*this._profit;
+        return `Profit is ${this._profitAmount}`;
+    }
+
+    adjustPrice(amount){
+        if (typeof amount == "number") {
+            this._price += amount;
+        } else {
+            throw "Amount should be a number";
+        }
+    }
+}
+
+class TaxableBook extends Book{
+    _taxRate
+    constructor(title, cost, profit, taxRate) {
+        super(title, cost, profit);
+        if (typeof taxRate != "number"){
+            throw "Profit must be number";
+        }else if (taxRate < 0){
+            throw "Profit must be >0 and <=0.5";
+        }else {
+            this._taxRate = taxRate;
+        }
+        this._price = (this._cost / (1 - this._profit - (this._taxRate/100))).toFixed(2);
+    }
+}
+
+const book1 = new Book("The Power of Habits", 14, 0.3)
+console.log(book1.getPrice());
+console.log(book1.getProfit());
+book1.adjustPrice(-3);
+console.log("After adjust then", book1.getPrice());
+
+const book2 = new TaxableBook("The Power of Habits", 14, 0.3, 24)
+console.log("With tax rate,", book2.getPrice());
